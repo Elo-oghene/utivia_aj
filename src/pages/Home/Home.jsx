cb522eedaa8a57da13f381e6a1519f97419ab4a5
Elo-oghene <olokorelo52.gmail.com>
2023-04-06 15:49:51 +0100

    Router has been added

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from "./style.module.css"
+
+const Home = () => {
+  return (
+    <div className={classes.holder}>Home</div>
+  )
+}
+
+export default Home
---
+++ src/pages/schools/School.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from "./schoolstyle.module.css"
+
+const School = () => {
+  return (
+    <div className={classes.holder}>School</div>
+  )
+}
+
+export default School
